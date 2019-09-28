5121c8a669901153af2faf4a88b900feca70d333
VladkaLipskaja <33036348+VladkaLipskaja@users.noreply.github.com>
2019-09-28 16:03:24 +0300

    default set ups

--- Photohack.Api/Controllers/ValuesController.cs
+++ Photohack.Api/Controllers/ValuesController.cs
@@ -14,7 +14,45 @@ namespace Photohack.Api.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            //            static void Main(string[] args)
+            //            {
+            //                var dtContent = GetDataTableFromExcel(@”c:\temp\test.xlsx”);
+            //                //var res = from DataRow dr in dtContent.Rows
+            //                // where (string)dr[“Name”] == “Gil”
+            //                // select ((string)dr[“Section”]).FirstOrDefault();
+            //                foreach (DataRow dr in dtContent.Rows)
+            //                {
+            //                    Console.WriteLine(dr[“Name”].ToString());
+            //                }
+            //                Console.ReadLine();
+            //            }
+            //            private static DataTable GetDataTableFromExcel(string path, bool hasHeader = true)
+            //            {
+            //                using (var pck = new OfficeOpenXml.ExcelPackage())
+            //                {
+            //                    using (var stream = File.OpenRead(path))
+            //                    {
+            //                        pck.Load(stream);
+            //                    }
+            //                    var ws = pck.Workbook.Worksheets.First();
+            //                    DataTable tbl = new DataTable();
+            //                    foreach (var firstRowCell in ws.Cells[1, 1, 1, ws.Dimension.End.Column])
+            //                    {
+            //                        tbl.Columns.Add(hasHeader ? firstRowCell.Text : string.Format(“Column { 0}”, firstRowCell.Start.Column));
+            //        }
+            //        var startRow = hasHeader ? 2 : 1;
+            //for (int rowNum = startRow; rowNum <= ws.Dimension.End.Row; rowNum++)
+            //{
+            //var wsRow = ws.Cells[rowNum, 1, rowNum, ws.Dimension.End.Column];
+            //        DataRow row = tbl.Rows.Add();
+            //foreach (var cell in wsRow)
+            //{
+            //row[cell.Start.Column — 1] = cell.Text;
+            //}
+            //}
+            //return tbl;
+            //}
+            return new string[] { "v", "v1" };
         }
 
         // GET api/values/5
---
+++ Photohack.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Photohack.Services
+{
+    public class Class1
+    {
+    }
+}
